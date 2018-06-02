64a56df59b7e99ebba2dc53021c1a66a4538e740
sukhpreet <sukhpreetkaur00@gmail.com>
2018-06-02 15:05:17 +1200

    first commit

---
+++ EntityFrameworkDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new EntityFrameworkDemoEntities();
+            var emp = new Employee()
+            {
+                EmployeeId = 1,
+                EmployeeName = "Sukhpreet",
+                Gender = "Female",
+                Salary = 2000
+            };
+            context.Employees.Add(emp);
+            context.SaveChanges();
+        }
+    }
+}
